3a38d5e116b64e75549b6ae05249d9bd1e30229e
Ondřej Mach <116495265+MachOndrej@users.noreply.github.com>
2023-06-27 12:53:42 +0200

    Update SQLConfig.java

--- src/main/java/configs/SQLConfig.java
+++ src/main/java/configs/SQLConfig.java
@@ -2,10 +2,10 @@
 package configs;
 
 public class SQLConfig {
-    public final static String SERVER_NAME = "147.230.21.34";
-    public final static String DATABASE_NAME = "RDB2023_MartinKonak";
-    public final static int PORT = 1433;
-    public final static String USERNAME = "student";
-    public final static String PASSWORD = "student";
+    public final static String SERVER_NAME = "";
+    public final static String DATABASE_NAME = "";
+    public final static int PORT = ;
+    public final static String USERNAME = "";
+    public final static String PASSWORD = "";
 
 }
